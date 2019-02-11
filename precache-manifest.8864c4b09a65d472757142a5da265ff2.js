549ea3b2bd123979459cc9b4b684cf929d1fb379
Ganeshram Mahalingam <ganesram.m@gmail.com>
2019-02-12 00:55:26 +0530

    Updates

---
+++ precache-manifest.8864c4b09a65d472757142a5da265ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "d22b646db24bea9feff1",
+    "url": "/portfolio/static/js/main.d22b646d.chunk.js"
+  },
+  {
+    "revision": "35154c60be4ab3bafde8",
+    "url": "/portfolio/static/js/1.35154c60.chunk.js"
+  },
+  {
+    "revision": "d22b646db24bea9feff1",
+    "url": "/portfolio/static/css/main.ec24d479.chunk.css"
+  },
+  {
+    "revision": "35154c60be4ab3bafde8",
+    "url": "/portfolio/static/css/1.6a3a3bbf.chunk.css"
+  },
+  {
+    "revision": "2b549cdc8804ca9689975b49a6ca80e0",
+    "url": "/portfolio/index.html"
+  }
+];
